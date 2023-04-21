f5d1e396051f210ce0376fd197ea55ef0d0b4fc5
nicolevenachi <ludwing@unicauca.edu.co>
2023-04-20 19:33:24 -0500

    inicio mapa

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 
+import GoogleMapReact from 'google-map-react';
+
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+
+import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined"
+import Rating from "@material-ui/lab"
+
+import useStyles from './sytles'
+
+
+
 function Map() {
+
+  const classes = useStyles()
+
+  //hook media query, será false si el query es menor que esto
+  const isMobile = useMediaQuery('(min-width: 600px)')
+
+  const coordinates = { lat: 0, lng: 0 }
+
   return (
-    <div>
+    <div className='classes.mapContainer'>
+      <GoogleMapReact
+        // key de ggogle map
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+
+        margin={[50, 50, 50, 50]}
+
+        options={''}
+
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
       <h1> Map </h1>
     </div>
   )
